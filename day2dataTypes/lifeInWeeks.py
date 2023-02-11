3102e1a1f4021e1e650f7313ef9f57bd0d1a37f3
Andre Jarboe <andrejarboe@gmail.com>
2023-02-11 18:06:42 -0500

    life works

--- day2dataTypes/lifeInWeeks.py
+++ day2dataTypes/lifeInWeeks.py
@@ -1,10 +1,25 @@
 # Create a program using maths and f-Strings that tells us how many days, weeks, months we have left if we live until 90 years old.
 
+# Example Input
+# 56
+# Example Output
+# You have 12410 days, 1768 weeks, and 408 months left.
+
 # 🚨 Don't change the code below 👇
-# age = input("What is your current age? ")
+age = input("What is your current age? ")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
+age = int(age)
 
-# num manipulation
-print(round(8/3, 2))
+
+days = 365
+weeks = 52
+months = 12
+
+years_left = 90 - age
+days_left = years_left * days
+weeks_left = years_left * weeks
+months_left = years_left * months
+
+print(f"You have {days_left} days, {weeks_left} weeks, and {months_left} months left")
