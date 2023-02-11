61ba0cae24fc28b3e3fa93198ec505d6a5b3314a
Andre Jarboe <andrejarboe@gmail.com>
2023-02-11 16:52:07 -0500

    notes

---
+++ day2dataTypes/notes.py
@@ -0,0 +1,8 @@
+# num manipulation
+print(round(8/3, 2))
+
+print ( 8/5) #1.6
+
+# /floor 
+print ( 8//5) #1
+print ( 8//3) #2
---
+++ day2dataTypes/oldman.py
@@ -0,0 +1,10 @@
+# Create a program using maths and f-Strings that tells us how many days, weeks, months we have left if we live until 90 years old.
+
+# 🚨 Don't change the code below 👇
+# age = input("What is your current age? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+# num manipulation
+print(round(8/3, 2))
