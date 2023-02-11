5e95b74aa169952c9b61f00b4d1fb8b7220cd9de
Andre Jarboe <andrejarboe@gmail.com>
2023-02-11 17:09:41 -0500

    f string

--- day2dataTypes/notes.py
+++ day2dataTypes/notes.py
@@ -6,3 +6,17 @@ print ( 8/5) #1.6
 # /floor 
 print ( 8//5) #1
 print ( 8//3) #2
+
+result = 4 / 2
+
+result /= 2
+
+print(result)
+score = 0
+
+score += 1
+
+print(score)
+
+#f string
+print(f"your score is: {score}" )
