e375982dbec0a8888c4e0ef687e7a5a395560cd4
Andre Jarboe <andrejarboe@gmail.com>
2023-02-11 18:44:39 -0500

    notes

---
+++ day3controlFlow/notes.py
@@ -0,0 +1,20 @@
+# if condition:
+#     do this
+# else:
+#     do this
+
+water_level = 50
+
+if water_level > 80:
+    print("Drain water")
+else:
+    print("Continue")
+
+print("welcome to the rollercoaster!")
+height = int(input("what is you height in cm?"))
+
+if height >= 120:
+    print("You can ride the rollercoaster!")
+else:
+    print("Sorry, you need to grow taller before you can ride.")
+
